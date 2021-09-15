504dcebadcfb17116a24c5b437c985305b7b8c45
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-09-15 17:22:11 -0300

    gerando components inputs para o form de cadastro

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ValidarCamposService } from "src/app/shared/components/campo/validar-campos.service";
 
 @Component({
-  selector: 'dio-cadastro-filmes',
-  templateUrl: './cadastro-filmes.component.html',
-  styleUrls: ['./cadastro-filmes.component.scss']
+  selector: "dio-cadastro-filmes",
+  templateUrl: "./cadastro-filmes.component.html",
+  styleUrls: ["./cadastro-filmes.component.scss"],
 })
 export class CadastroFilmesComponent implements OnInit {
-
   cadastro: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    public validacao: ValidarCamposService,
+    private fb: FormBuilder
+  ) {}
 
   get f() {
     return this.cadastro.controls;
   }
 
   ngOnInit(): void {
-
     this.cadastro = this.fb.group({
-      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
-      urlFoto: ['', [Validators.minLength(10)]],
-      dtLancamento: ['', [Validators.required]],
-      descricao: [''],
+      titulo: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
+        ],
+      ],
+      urlFoto: ["", [Validators.minLength(10)]],
+      dtLancamento: ["", [Validators.required]],
+      descricao: [""],
       nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
-      urlIMDb: ['', [Validators.minLength(10)]],
-      genero: ['', [Validators.required]]
+      urlIMDb: ["", [Validators.minLength(10)]],
+      genero: ["", [Validators.required]],
     });
-
   }
 
   salvar(): void {
@@ -36,11 +44,10 @@ export class CadastroFilmesComponent implements OnInit {
       return;
     }
 
-    alert('SUCESSO!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+    alert("SUCESSO!!\n\n" + JSON.stringify(this.cadastro.value, null, 4));
   }
 
   reiniciarForm(): void {
     this.cadastro.reset();
   }
-
 }
--- src/app/filmes/filmes.module.ts
+++ src/app/filmes/filmes.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
-import { MaterialModule } from '../shared/material/material.module';
-import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.component';
+import { CadastroFilmesComponent } from "./cadastro-filmes/cadastro-filmes.component";
+import { MaterialModule } from "../shared/material/material.module";
+import { ListagemFilmesComponent } from "./listagem-filmes/listagem-filmes.component";
+import { CampoModule } from "../shared/components/campo/campo.module";
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.compo
     MaterialModule,
     ReactiveFormsModule,
     FormsModule,
+    CampoModule,
   ],
-  declarations: [CadastroFilmesComponent, ListagemFilmesComponent]
+  declarations: [CadastroFilmesComponent, ListagemFilmesComponent],
 })
-export class FilmesModule { }
+export class FilmesModule {}
