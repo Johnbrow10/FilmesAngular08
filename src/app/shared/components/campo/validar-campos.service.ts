66d512d67a8c6308f0164ea85c9a5121b04ecd2f
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-09-15 22:11:31 -0300

    mensagens com errros dinamicos nso inputs

--- src/app/shared/components/campo/validar-campos.service.ts
+++ src/app/shared/components/campo/validar-campos.service.ts
@@ -1,21 +1,28 @@
-import { Injectable } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import { Injectable } from "@angular/core";
+import { AbstractControl } from "@angular/forms";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ValidarCamposService {
-
-  constructor() { }
+  constructor() {}
 
   hasErrorValidar(control: AbstractControl, errorName: string): boolean {
-    if ((control.dirty || control.touched) && this.hasError(control, errorName)) {
+    if (
+      (control.dirty || control.touched) &&
+      this.hasError(control, errorName)
+    ) {
       return true;
     }
-    return false
+    return false;
   }
 
   hasError(control: AbstractControl, errorName: string): boolean {
-    return control.hasError(errorName)
+    return control.hasError(errorName);
+  }
+
+  lengthValidar(control: AbstractControl, errorName: string): number {
+    const error = control.errors[errorName];
+    return error.requiredLength || error.min || error.max || 0;
   }
 }
