c80583675e1e0f850d4f739aa7576fd535422e5a
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-09-20 17:30:13 -0300

    funções para scroll infinito e listagem dos filmes com filtros e ordem de cadastro

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,19 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { FilmeService } from "src/app/core/filmes.service";
+import { Filme } from "src/app/shared/models/filme";
 
 @Component({
-  selector: 'dio-listagem-filmes',
-  templateUrl: './listagem-filmes.component.html',
-  styleUrls: ['./listagem-filmes.component.scss']
+  selector: "dio-listagem-filmes",
+  templateUrl: "./listagem-filmes.component.html",
+  styleUrls: ["./listagem-filmes.component.scss"],
 })
 export class ListagemFilmesComponent implements OnInit {
+  readonly qtdPagina = 4;
+  pagina: number = 0;
+  texto: string = "";
+  genero: string = "";
+  filmes: Filme[] = [];
+  filtrosLisatgem: FormGroup;
+  generos: Array<string>;
 
-  constructor() { }
+  constructor(private filmeService: FilmeService, private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.filtrosLisatgem = this.fb.group({
+      texto: [""],
+      genero: [""],
+    });
 
+    this.filtrosLisatgem.get("texto").valueChanges.subscribe((val: string) => {
+      this.texto = val;
+      this.resetarConsulta();
+    });
+    this.filtrosLisatgem.get("genero").valueChanges.subscribe((val: string) => {
+      this.genero = val;
+      this.resetarConsulta();
+    });
+
+    this.generos = [
+      "Ação",
+      "Aventura",
+      "Comédia",
+      "Drama",
+      "Ficção Cientifica",
+      "Romance",
+      "Terror",
+    ];
+
+    this.listarFilmes();
   }
 
-  open() {
+  onScroll(): void {
+    this.listarFilmes();
   }
 
+  private listarFilmes(): void {
+    this.pagina++;
+    this.filmeService
+      .listar(this.pagina, this.qtdPagina, this.texto, this.genero)
+      .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
+  }
 }
