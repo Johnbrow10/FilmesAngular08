b813054b959ab2f4f1cd815a8dd1eaba83c486d4
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-09-20 17:25:00 -0300

    add filtros e e ordem para filmes nas listagens

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Filme } from "../shared/models/filme";
 
@@ -11,6 +11,27 @@ export class FilmeService {
   constructor(private http: HttpClient) {}
 
   salvar(filme: Filme): Observable<Filme> {
-    return this.http.post<Filme>(url, filme)
+    return this.http.post<Filme>(url, filme);
+  }
+
+  listar(
+    pagina: number,
+    qtdPagina: number,
+    texto: string,
+    genero: string
+  ): Observable<Filme[]> {
+    //  listando os filmes mas com limitação de pagina
+    let httpParams = new HttpParams();
+    httpParams = httpParams.set("_page", pagina.toString());
+    httpParams = httpParams.set("_limit", qtdPagina.toString());
+    httpParams = httpParams.set("_order", "desc");
+    httpParams = httpParams.set("_sort", "id");
+    if (texto) {
+      httpParams = httpParams.set("q", texto);
+    }
+    if (genero) {
+      httpParams = httpParams.set("genero", genero);
+    }
+    return this.http.get<Filme[]>(url, { params: httpParams });
   }
 }
