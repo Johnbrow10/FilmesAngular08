9edec6379cc23d36d5ca96d4150b29d751c2f165
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-09-21 17:20:12 -0300

    desmenebrando o form para ultilizar edicao nele

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MatDialog } from "@angular/material";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { FilmeService } from "src/app/core/filmes.service";
 import { AlertaComponent } from "src/app/shared/components/alerta/alerta.component";
 import { ValidarCamposService } from "src/app/shared/components/campo/validar-campos.service";
@@ -16,12 +16,15 @@ import { Filme } from "src/app/shared/models/filme";
 export class CadastroFilmesComponent implements OnInit {
   cadastro: FormGroup;
   generos: Array<string>;
+  id: number;
+
   constructor(
     public validacao: ValidarCamposService,
     public dialog: MatDialog,
     private fb: FormBuilder,
     private filmeService: FilmeService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {}
 
   get f() {
@@ -29,6 +32,17 @@ export class CadastroFilmesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params["id"];
+    // if para caso ja estiver um filme sera usado e os campos serao preenchidos
+    // com esses dados para uso da edição de tal filme
+    if (this.id) {
+      this.filmeService
+        .visualizar(this.id)
+        .subscribe((filme: Filme) => this.criarFormulario(filme));
+    } else {
+      this.criarFormulario(this.criarFilmeEmBranco());
+    }
+
     this.generos = [
       "Ação",
       "Aventura",
@@ -38,23 +52,6 @@ export class CadastroFilmesComponent implements OnInit {
       "Romance",
       "Terror",
     ];
-
-    this.cadastro = this.fb.group({
-      titulo: [
-        "",
-        [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(256),
-        ],
-      ],
-      urlFoto: ["", [Validators.minLength(10)]],
-      dtLancamento: ["", [Validators.required]],
-      descricao: [""],
-      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
-      urlIMDb: ["", [Validators.minLength(10)]],
-      genero: ["", [Validators.required]],
-    });
   }
 
   submit(): void {
@@ -69,6 +66,41 @@ export class CadastroFilmesComponent implements OnInit {
 
   reiniciarForm(): void {
     this.cadastro.reset();
+  }
+
+  private criarFormulario(filme: Filme): void {
+    this.cadastro = this.fb.group({
+      titulo: [
+        filme.titulo,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
+        ],
+      ],
+      urlFoto: [filme.urlFoto, [Validators.minLength(10)]],
+      dtLancamento: [filme.dtLancamento, [Validators.required]],
+      descricao: [filme.descricao],
+      nota: [
+        filme.nota,
+        [Validators.required, Validators.min(0), Validators.max(10)],
+      ],
+      urlIMDb: [filme.urlIMDb, [Validators.minLength(10)]],
+      genero: [filme.genero, [Validators.required]],
+    });
+  }
+
+  private criarFilmeEmBranco(): Filme {
+    return {
+      id: null,
+      titulo: null,
+      dtLancamento: null,
+      urlFoto: null,
+      descricao: null,
+      nota: null,
+      urlIMDb: null,
+      genero: null,
+    } as Filme;
   }
 
   private salvar(filme: Filme): void {
@@ -95,13 +127,13 @@ export class CadastroFilmesComponent implements OnInit {
         const config = {
           data: {
             titulo: "Erro ao Salvar o registro",
-            descricao: "Não conseguimos salvar seu registro, favor tentar novamente mais tarde",
-            corBtnSucesso:"warn",
+            descricao:
+              "Não conseguimos salvar seu registro, favor tentar novamente mais tarde",
+            corBtnSucesso: "warn",
             btnSucesso: "Fechar",
-
           } as Alerta,
         };
-        this.dialog.open(AlertaComponent,config);
+        this.dialog.open(AlertaComponent, config);
       }
     );
   }
